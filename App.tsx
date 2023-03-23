19831e83cd8aa83e06251f939b193059e37509a5
LucasAlencar <lucas.linard@outlook.com>
2023-03-23 11:22:59 -0300

    fix: import theme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/theme";
+import  theme  from "./src/theme";
+
+
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
