36c18db5a4b54268967f955b9381708b3b6ff233
LucasAlencar <lucas.linard@outlook.com>
2023-03-23 12:06:22 -0300

    feat: Home screen

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export const Container = styled(SafeAreaView)`
+    flex: 1;
+    padding: 24px;
+  
+    align-items: center;
+
+    background-color: ${({ theme }) => theme.COLORS.WHITE};
+`
