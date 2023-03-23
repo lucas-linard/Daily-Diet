5e19fc2424020c281dbc7fbd74f140033f99abec
LucasAlencar <lucas.linard@outlook.com>
2023-03-23 10:50:17 -0300

    feat: Theme file

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export const theme = {
+    COLORS:{
+        RED_DARK: '#BF3B44',
+        RED_MID: '#F3BABD',
+        RED_LIGHT: '#F4E6E7',
+
+        GREEN_DARK: '#639339',
+        GREEN_MID: '#CBE4B4',
+        GREEN_LIGHT: '#E5F0DB',
+
+        GRAY100: '#1B1D1E',
+        GRAY200: '#333638',
+        GRAY300: '#5C6265',
+        GRAY400: '#B9BBBC',
+        GRAY500: '#DDDEDF',
+        GRAY600: '#EFF0F0',
+        GRAY700: '#FAFAFA',
+
+        WHITE : '#FFFFFF',
+    },
+    FONT_FAMILY: {
+        REGULAR: "NunitoSans_400Regular",
+        BOLD: "NunitoSans_700Bold",
+      },
+    FONT_SIZE:{
+        XS: 12,
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL2: 20,
+        XL4: 24,
+    }
+}
