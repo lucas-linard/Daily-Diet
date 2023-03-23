a679e1bee4d739dcd3f317b9165089ca8ffcd737
LucasAlencar <lucas.linard@outlook.com>
2023-03-23 12:06:48 -0300

    feat: Load custom fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
 
 import { ThemeProvider } from "styled-components/native";
-import  theme  from "./src/theme";
+import theme from "./src/theme";
 
+import { Loading } from "@components/Loading";
+import Home from "@screens/Home";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
+      {fontsLoaded ? <Home /> : <Loading /> }
     </ThemeProvider>
   );
 }
