5ac579931117897c51718d5bdb4c0502672258f6
LucasAlencar <lucas.linard@outlook.com>
2023-03-27 19:23:33 -0300

    feat: BackIcon

--- src/components/Percent/index.tsx
+++ src/components/Percent/index.tsx
@@ -1,19 +1,31 @@
-import { Container, ArrowUp } from "./styles";
+import { Container, Box, ArrowUpRightIcon, BackIcon } from "./styles";
 
 import { Text } from "@components/Text";
 
 type PercentProps = {
-    percent: number;
-    healthy?: boolean;
-    showArrow?: boolean;
-}
+  percent: number;
+  healthy?: boolean;
+  showBackButton?: boolean;
+};
 
-export function Percent({ percent, healthy = true, showArrow = true}: PercentProps) {
-    return (
-        <Container healthy={healthy}>
-            <Text fontSize="XL4" fontFamily="BOLD">{percent}%</Text>
-            <Text fontSize="SM">das refeições dentro da dieta</Text>
-            {showArrow && <ArrowUp healthy={healthy}/>}
-        </Container>
-    );
+export function Percent({
+  percent,
+  healthy = true,
+  showBackButton = false,
+}: PercentProps) {
+  return (
+    <Container healthy={healthy}>
+      {showBackButton ? (
+        <Box>
+          <BackIcon healthy={healthy} />
+        </Box>
+      ) : (
+        <ArrowUpRightIcon healthy={healthy} />
+      )}
+      <Text fontSize="XL4" fontFamily="BOLD">
+        {percent}%
+      </Text>
+      <Text fontSize="SM">das refeições dentro da dieta</Text>
+    </Container>
+  );
 }
--- src/components/Percent/styles.tsx
+++ src/components/Percent/styles.tsx
@@ -1,11 +1,14 @@
 import styled, { css } from "styled-components/native";
 import { IconProps } from "phosphor-react-native";
 import { ArrowUpRight } from "phosphor-react-native";
+import { ArrowLeft } from "phosphor-react-native";
+
 import { Pressable } from "react-native";
 
 
 type PercentProps = {
   healthy?: boolean;
+  showBackButton?: boolean;
 };
 
 type ArrowUpProps = IconProps & {
@@ -14,23 +17,31 @@ healthy?: boolean;
  
 export const Container = styled(Pressable)<PercentProps>`
   width: 100%;
-  justify-content: center;
   align-items: center;
+  justify-content: ${({ showBackButton }) => showBackButton ? 'flex-start' : 'center'};
 
   min-height: 102px;
   max-height: 102px;
 
-  padding: 20px 16px;
   gap: 2px;
-  margin-bottom: 40px;
-
+  
   border-radius: 8px;
 
   background-color: ${({ theme, healthy }) =>
     healthy ? theme.COLORS.GREEN_LIGHT : theme.COLORS.RED_LIGHT};
 `;
 
-export const ArrowUp = styled(ArrowUpRight).attrs<ArrowUpProps>(({ theme, healthy }) => ({
+export const Box = styled.View`
+  width: 100%;
+  flex-direction: row;
+`
+
+export const BackIcon = styled(ArrowLeft).attrs<ArrowUpProps>(({ theme, healthy }) => ({
+  color: healthy ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK,
+  size: 24,
+}))<ArrowUpProps>``;
+
+export const ArrowUpRightIcon = styled(ArrowUpRight).attrs<ArrowUpProps>(({ theme, healthy }) => ({
   color: healthy ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK,
   size: 24,
 }))<ArrowUpProps>`
