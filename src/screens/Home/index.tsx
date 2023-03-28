1132a345d41e5b291c636fa94b671ea8b9e5450a
LucasAlencar <lucas.linard@outlook.com>
2023-03-27 23:58:35 -0300

    feat: HomeScreen content

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,23 @@
-import { Container } from "./styles";
+import { Container , Box } from "./styles";
 
+import { Text } from "@components/Text";
 import { Header } from "@components/Header";
-
+import { Percent } from "@components/Percent";
+import { Button } from "@components/Button";
+import { Meal } from "@components/Meal";
 export default function Home() {
-    return (
-        <Container>
-            <Header />
-        </Container>
-    );
+  return (
+    <Container>
+      <Header />
+      <Percent percent={90.86}/>
+      <Box style={{marginTop: 40}}>
+        <Text>Refeições</Text>
+        <Button title="Nova refeição" Icon="Plus"/>
+      </Box>
+      <Box style={{marginTop: 32}}>
+        <Text fontFamily="BOLD" fontSize="LG">12.08.22</Text>
+        </Box>
+      <Meal />
+    </Container>
+  );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,26 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export const Container = styled(SafeAreaView)`
-    flex: 1;
-    padding: 24px;
-  
-    align-items: center;
 
-    background-color: ${({ theme }) => theme.COLORS.WHITE};
-`
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  padding: 24px;
+
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  
+`;
+
+export const Text = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY100};
+  `}
+`;
+
+export const Box = styled.View`
+    width: 100%;
+    gap: 8px;
+`;
