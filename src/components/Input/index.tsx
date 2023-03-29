017d0e120d29699a3e8f4529e26a6f2d0d1ed6da
LucasAlencar <lucas.linard@outlook.com>
2023-03-29 15:41:17 -0300

    feat: multiline input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,21 +9,26 @@ import { Text } from "@components/Text";
 type InputProps = TextInputProps & {
   label: string;
   inputRef?: React.RefObject<TextInput>;
+  numberOfLines?: number;
 };
 
 function handleIsFilled(value: string) {
   return value.trim().length > 0;
 }
 
-export const Input = ({ label, value = "", inputRef ,...rest }: InputProps) => {
+export const Input = ({ label, value = "", inputRef, numberOfLines = 1 , ...rest }: InputProps) => {
   const [active, setActive] = useState(false);
   
   function handleIsActive (){
     setActive(!active);
   }
 
+  if(numberOfLines > 3){
+    throw new Error("Number of lines must be less than 3");
+  }
+
   return (
-    <Container>
+    <Container numberOfLines={numberOfLines}>
       <Text fontSize="SM" fontFamily="BOLD" color="GRAY200">
         {label}
       </Text>
@@ -34,6 +39,8 @@ export const Input = ({ label, value = "", inputRef ,...rest }: InputProps) => {
         value={value}
         onEndEditing={handleIsActive}
         onFocus={handleIsActive}
+        multiline={numberOfLines > 1}
+        numberOfLines={numberOfLines}
         {...rest} 
       />
     </Container>
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,24 +4,31 @@ import styled, {css} from 'styled-components/native';
 export type TextFieldStyleProps = {
     active: boolean;
     filled: boolean;
+    numberOfLines: number;
 }
 
-export const Container = styled.View`
+type ContainerProps = {
+    numberOfLines: number;
+}
+
+export const Container = styled.View<ContainerProps>`
     flex: 1;
 
     min-height: 70px;
-    max-height: 70px;
+    /* por conta do flex ele esta crescendo o maximo que pode */
+    max-height: ${({numberOfLines }) => numberOfLines * 30 + 48}px;;
 
     gap: 4px;
 `
 
 export const TextField = styled(TextInput)<TextFieldStyleProps>`
     width: 100%;
-
+    height: ${({numberOfLines }) => numberOfLines * 30 + 28}px;
+    
     border: 1px solid ${({theme}) => theme.COLORS.GRAY500};
     border-radius: 6px;
     padding: 14px;
-    gap:8px;
+    gap: 108px;
 
     ${({theme}) => css`
         font-size: ${theme.FONT_SIZE.MD}px;
