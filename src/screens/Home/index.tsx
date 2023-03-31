02f8f7a04b96f4dbd8ce88880e43e67dedc8193b
LucasAlencar <lucas.linard@outlook.com>
2023-03-31 12:13:00 -0300

    fix: Button width on home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Percent percent={90.86}/>
       <Box style={{marginTop: 40}}>
         <Text>Refeições</Text>
-        <Button title="Nova refeição" Icon="Plus"/>
+        <Button title="Nova refeição" Icon="Plus" fullWidth/>
       </Box>
       <Box style={{marginTop: 32}}>
         <Text fontFamily="BOLD" fontSize="LG">12.08.22</Text>
