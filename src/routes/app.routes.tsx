53a26f7193a37c056135cdb7bb9edafff477bd4c
LucasAlencar <lucas.linard@outlook.com>
2023-03-31 12:14:51 -0300

    feat: new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,10 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "@screens/Home";
+import Stats from "@screens/Stats";
+import { MealForm } from "@screens/MealForm";
+import  Feedback  from "@screens/Feedback";
+import Details from "@screens/Details";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -8,6 +12,10 @@ export function AppRoutes() {
     return (
       <Navigator screenOptions={{headerShown: false}}>
         <Screen name="Home" component={Home} />
+        <Screen name="Stats" component={Stats} />
+        <Screen name="MealForm" component={MealForm} />
+        <Screen name="Feedback" component={Feedback} />
+        <Screen name="Details" component={Details} />
       </Navigator>
     );
   }
