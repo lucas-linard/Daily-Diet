90ce3042fee7a0c46a46378ac1604c4f4bf3717e
LucasAlencar <lucas.linard@outlook.com>
2023-04-03 16:24:57 -0300

    feat: MealForm form validation

--- src/screens/MealForm/index.tsx
+++ src/screens/MealForm/index.tsx
@@ -1,27 +1,65 @@
 import { useState, useRef } from "react";
 import { TextInput } from "react-native";
-import { Container, Card, RowBox, EndBox, DateTimeBox } from "./styles";
+import { useForm, Controller } from "react-hook-form";
+
+import { Container, Card, RowBox, EndBox } from "./styles";
 
 import { Header } from "@components/Header";
 import { Input } from "@components/Input";
 import { Select } from "@components/Select";
 import { Button } from "@components/Button";
-
 import { DateTimePicker } from "@components/DateTimePicker";
+import { Text } from "@components/Text";
+
+type FormData = {
+  name: string;
+  description: string;
+  isOnDiet: "YES" | "NO";
+  date: Date;
+  time: Date;
+};
 
 export function MealForm() {
-  const [name, setName] = useState("");
-  const [isOnDiet, setIsOnDiet] = useState<"YES" | "NO">("YES");
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const {
+    control,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      name: "",
+      description: "",
+      isOnDiet: "YES",
+      date: new Date(),
+      time: new Date(),
+    },
+  });
+
+  const watchDate = watch("date");
+  const watchTime = watch("time");
+
   const nameInputRef = useRef<TextInput>(null);
   const [isDatePickerVisible, setDatePickerVisibility] = useState({
     date: false,
     time: false,
   });
 
-  const handleSelectChange = (value: "YES" | "NO") => {
-    setIsOnDiet(value);
-  };
+  function validateTime() {
+    let dataTime = new Date(
+      watchDate.getFullYear(),
+      watchDate.getMonth(),
+      watchDate.getDate(),
+      watchTime.getHours(),
+      watchTime.getMinutes(),
+      watchTime.getSeconds()
+    );
+
+    return dataTime <= new Date();
+  }
+
+  function handleSubmitForm(data: any) {
+    console.log(data);
+  }
 
   function handleDatePress(value: "date" | "time") {
     value === "date"
@@ -33,51 +71,127 @@ export function MealForm() {
     setDatePickerVisibility({ time: false, date: false });
   }
 
-  const handleConfirmDate = (date: Date) => {
-    setSelectedDate(date);
-    handleHideDatePicker();
-  };
-
   return (
     <Container edges={["top", "left", "right"]}>
       <Header title="Nova refeição" />
       <Card>
-        <Input
-          inputRef={nameInputRef}
-          label="Nome"
-          value={name}
-          onChangeText={setName}
+        <Controller
+          control={control}
+          name="name"
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <>
+              <Input
+                inputRef={nameInputRef}
+                label="Nome"
+                value={value}
+                onChangeText={onChange}
+              />
+            </>
+          )}
         />
-        <Input
-          label="Descrição"
-          value={name}
-          onChangeText={setName}
-          numberOfLines={3}
+        {errors.name && (
+          <Text
+            fontFamily="BOLD"
+            fontSize="SM"
+            color="RED_DARK"
+            style={{ marginTop: -15 }}
+          >
+            Preencha o nome da sua refeição*
+          </Text>
+        )}
+        <Controller
+          control={control}
+          name="description"
+          rules={{
+            required: false,
+            maxLength: 100,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              label="Descrição"
+              value={value}
+              onChangeText={onChange}
+              numberOfLines={3}
+            />
+          )}
         />
+
         <RowBox>
-          <DateTimePicker
-            isVisible={isDatePickerVisible.date}
-            onPress={() => handleDatePress("date")}
-            label="Data"
-            value={selectedDate}
-            maximumDate={new Date()}
-            mode="date"
-            onConfirm={handleConfirmDate}
-            onCancel={handleHideDatePicker}
+          <Controller
+            control={control}
+            name="date"
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, value } }) => (
+              <DateTimePicker
+                isVisible={isDatePickerVisible.date}
+                onPress={() => handleDatePress("date")}
+                label="Data"
+                value={value}
+                maximumDate={new Date()}
+                mode="date"
+                onConfirm={(e) => {
+                  onChange(e);
+                  handleHideDatePicker();
+                }}
+                onCancel={handleHideDatePicker}
+              />
+            )}
           />
-          <DateTimePicker
-            isVisible={isDatePickerVisible.time}
-            onPress={() => handleDatePress("time")}
-            label="Hora"
-            value={selectedDate}
-            mode="time"
-            onConfirm={handleConfirmDate}
-            onCancel={handleHideDatePicker}
+
+          <Controller
+            control={control}
+            name="time"
+            rules={{
+              required: true,
+              validate: validateTime,
+            }}
+            render={({ field: { onChange, value } }) => (
+              <DateTimePicker
+                isVisible={isDatePickerVisible.time}
+                onPress={() => handleDatePress("time")}
+                label="Hora"
+                value={value}
+                mode="time"
+                onConfirm={(e) => {
+                  onChange(e);
+                  handleHideDatePicker();
+                }}
+                onCancel={handleHideDatePicker}
+              />
+            )}
           />
         </RowBox>
-        <Select value={isOnDiet} onChange={handleSelectChange} />
+        {errors.time && (
+          <Text
+            fontFamily="BOLD"
+            fontSize="SM"
+            color="RED_DARK"
+            style={{ marginTop: -5 }}
+          >
+            Informe uma data/hora valida!
+          </Text>
+        )}
+        <Controller
+          control={control}
+          name="isOnDiet"
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <Select value={value} onChange={onChange} />
+          )}
+        />
         <EndBox>
-          <Button title="Cadastrar refeição" fullWidth />
+          <Button
+            title="Cadastrar refeição"
+            fullWidth
+            onPress={handleSubmit(handleSubmitForm)}
+          />
         </EndBox>
       </Card>
     </Container>
