249a7bd6cf5297f41d860388725745f8067eb8c6
LucasAlencar <lucas.linard@outlook.com>
2023-04-04 12:54:21 -0300

    feat: Home screen - percent shared-element

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,28 +1,59 @@
-import { Container , Header, Profile ,Box } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { SharedElement } from "react-navigation-shared-element";
+ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import ProfileImage from '@assets/Profile.png';
+import { Container, Header, Profile, Box } from "./styles";
+
+import ProfileImage from "@assets/Profile.png";
 import { Text } from "@components/Text";
 import { Logo } from "@components/Logo";
-
 import { Percent } from "@components/Percent";
 import { Button } from "@components/Button";
 import { Meal } from "@components/Meal";
 export default function Home() {
+  const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
+  function handleNavigateToStats() {
+    navigation.navigate("Stats");
+  }
+
+  function handleNavigateToDetails() {
+    navigation.navigate("Details");
+  }
+
+  function handleNewMeal() {
+    navigation.navigate("MealForm");
+  }
+
   return (
-    <Container>
+    <Container
+    style={{  paddingTop: insets.top,
+      paddingLeft: insets.left + 24,
+      paddingRight: insets.right + 24,
+      paddingBottom: insets.bottom}}
+    >
       <Header>
-      <Logo />
-      <Profile source={ProfileImage} />
+        <Logo />
+        <Profile source={ProfileImage} />
       </Header>
-      <Percent percent={90.86}/>
-      <Box style={{marginTop: 40}}>
+      <SharedElement style={{ width: "100%" }} id="percent">
+        <Percent percent={90.86} onPress={handleNavigateToStats} />
+      </SharedElement>
+      <Box style={{ marginTop: 40 }}>
         <Text>Refeições</Text>
-        <Button title="Nova refeição" Icon="Plus" fullWidth/>
+        <Button
+          title="Nova refeição"
+          Icon="Plus"
+          fullWidth
+          onPress={handleNewMeal}
+        />
       </Box>
-      <Box style={{marginTop: 32}}>
-        <Text fontFamily="BOLD" fontSize="LG">12.08.22</Text>
-        </Box>
-      <Meal />
+      <Box style={{ marginTop: 32 }}>
+        <Text fontFamily="BOLD" fontSize="LG">
+          12.08.22
+        </Text>
+      </Box>
+      <Meal onPress={handleNavigateToDetails} />
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,9 @@
 import styled, { css } from "styled-components/native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 
-export const Container = styled(SafeAreaView)`
+export const Container = styled.View`
   flex: 1;
-  padding: 24px;
+  
 
   align-items: center;
 
