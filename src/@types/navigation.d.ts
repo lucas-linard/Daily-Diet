e17768de5dbe4320a89f995d2ad73f716df25036
LucasAlencar <lucas.linard@outlook.com>
2023-04-04 12:46:06 -0300

    feat: navigation types

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+    namespace ReactNavigation{
+        interface RootParamList{
+            Home: undefined;
+            Stats: undefined;
+            Details: undefined   ;
+            MealForm: undefined;
+            Feedback: undefined;
+
+        }
+    }
+}
