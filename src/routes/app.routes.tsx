e76f6b67d174d13c43481db91f06f66216571688
LucasAlencar <lucas.linard@outlook.com>
2023-04-04 12:46:35 -0300

    feat: shared element navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,21 +1,43 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createSharedElementStackNavigator } from "react-navigation-shared-element";
 
 import Home from "@screens/Home";
 import Stats from "@screens/Stats";
 import { MealForm } from "@screens/MealForm";
-import  Feedback  from "@screens/Feedback";
+import Feedback from "@screens/Feedback";
 import Details from "@screens/Details";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen } = createSharedElementStackNavigator();
 
 export function AppRoutes() {
-    return (
-      <Navigator screenOptions={{headerShown: false}}>
-        <Screen name="Home" component={Home} />
-        <Screen name="Stats" component={Stats} />
-        <Screen name="MealForm" component={MealForm} />
-        <Screen name="Feedback" component={Feedback} />
-        <Screen name="Details" component={Details} />
-      </Navigator>
-    );
-  }
+  return (
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        cardStyleInterpolator: ({ current }) => ({
+          cardStyle: {
+            opacity: current.progress,
+          },
+        }),
+      }}
+    >
+      <Screen name="Home" component={Home} />
+      <Screen
+        name="Stats"
+        component={Stats}
+        sharedElements={() => {
+          return [
+            {
+              id: `percent`,
+              animation: "fade",
+            },
+          ];
+        }}
+      />
+      <Screen name="MealForm" component={MealForm} />
+      <Screen name="Feedback" component={Feedback} />
+      <Screen name="Details" component={Details} />
+    </Navigator>
+  );
+}
