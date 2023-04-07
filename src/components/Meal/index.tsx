737c4e00612343b39383e6ebf20e6067b27f67ba
LucasAlencar <lucas.linard@outlook.com>
2023-04-07 11:59:34 -0300

    refactor: update component to receive data as props instead of using mock data

--- src/components/Meal/index.tsx
+++ src/components/Meal/index.tsx
@@ -2,17 +2,26 @@ import { PressableProps } from "react-native";
 import { Container, Dot } from "./styles";
 import { Text } from "@components/Text";
 
-export function Meal({...rest} : PressableProps) {
+export type isOnDietSelectType = 'YES' | 'NO';
+type Props = PressableProps & {
+  title: string;
+  date: Date;
+  isOnDiet:isOnDietSelectType;
+};
+export function Meal({ title, date, isOnDiet, ...rest }: Props) {
+  date = new Date(date);
+  const timeString = date.toLocaleTimeString("pt-BR", {hour: "2-digit", minute: "2-digit"});
+  
   return (
     <Container {...rest}>
-      <Text>20:00</Text>
+      <Text>{timeString}</Text>
       <Text color="GRAY400" fontSize="SM">
         |
       </Text>
       <Text style={{ width: "70%" }} color="GRAY200" numberOfLines={1}>
-        Vitamina de Banana com morango
+        {title}
       </Text>
-      <Dot />
+      <Dot isOnDiet={isOnDiet} />
     </Container>
   );
 }
--- src/components/Meal/styles.ts
+++ src/components/Meal/styles.ts
@@ -1,34 +1,37 @@
 import styled from "styled-components/native";
+import { isOnDietSelectType } from ".";
 import { Pressable } from "react-native";
 
-
+type DotStyleProps = {
+  isOnDiet: isOnDietSelectType;
+};
 export const Container = styled(Pressable)`
-    flex-direction: row;
-    align-items: center;
+  flex-direction: row;
+  align-items: center;
 
-    height: 49px;
-    width: 100%;
+  height: 49px;
+  width: 100%;
 
-    padding: 14px 16px;
-    gap: 12px;
+  padding: 14px 16px;
+  gap: 12px;
 
-    border: 1px;
-    border-color: ${({ theme }) => theme.COLORS.GRAY500};
-    border-radius: 6px;
+  border: 1px;
+  border-color: ${({ theme }) => theme.COLORS.GRAY500};
+  border-radius: 6px;
 `;
 
 export const TextContainer = styled.View`
-    width: 70%;
-    `;
+  width: 70%;
+`;
 
-export const Dot = styled.View`
-    position: absolute;
-    right: 12px;
-    
-    width: 14px;
-    height: 14px;
+export const Dot = styled.View<DotStyleProps>`
+  position: absolute;
+  right: 12px;
 
-    background-color: ${({ theme }) => theme.COLORS.RED_MID};
-    border-radius: 999px;
+  width: 14px;
+  height: 14px;
 
-`
+  background-color: ${({ theme, isOnDiet }) =>
+    isOnDiet === 'YES' ? theme.COLORS.GREEN_MID : theme.COLORS.RED_MID};
+  border-radius: 999px;
+`;
