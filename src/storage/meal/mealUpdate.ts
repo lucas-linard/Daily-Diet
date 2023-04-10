d71a089998a4a441041f9239983234525cffcf3c
LucasAlencar <lucas.linard@outlook.com>
2023-04-10 12:36:59 -0300

    fix: issue finding the meal

--- src/storage/meal/mealUpdate.ts
+++ src/storage/meal/mealUpdate.ts
@@ -7,28 +7,54 @@ type MealStorageType = {
   data: MealType[];
 };
 
+function findIndexes(id: string, data: MealStorageType[]) {
+  for (let i = 0; i < data.length; i++) {
+    const section = data[i].data;
+    for (let j = 0; j < section.length; j++) {
+      if (section[j].id === id) {
+        return { sectionIndex: i, mealIndex: j };
+      }
+    }
+  }
+  return null; // retorna null se o id não for encontrado
+}
+
 export async function mealUpdate(meal: MealType) {
-  console.log(meal);
   try {
     const storedMealsJSON = await AsyncStorage.getItem(MEAL_COLLECTION);
     if (storedMealsJSON) {
-      const storedMeals: MealStorageType[] = JSON.parse(storedMealsJSON);
-
+      let storedMeals: MealStorageType[] = JSON.parse(storedMealsJSON);
       const mealSection = meal.date.toLocaleDateString("pt-BR");
-      const indexOfSection = storedMeals.findIndex(
-        (section: MealStorageType) => section.title === mealSection
-      );
-      if (indexOfSection !== -1) {
-        const indexOfMeal = storedMeals[indexOfSection].data.findIndex(
-          (item: MealType) => item.id === meal.id
-        );
-        storedMeals[indexOfSection].data[indexOfMeal] = meal;
+
+      const indexes = findIndexes(meal.id, storedMeals);
+      if (indexes) {
+        if (storedMeals[indexes.sectionIndex].title !== mealSection) {
+          storedMeals[indexes.sectionIndex].data.length === 1
+          //deleta o meal ou a section inteira se for o único meal
+            ? storedMeals.splice(indexes.sectionIndex, 1) //  ✅
+            : storedMeals[indexes.sectionIndex].data.splice(indexes.mealIndex, 1);//  ✅
+
+            // encontra o index da section que o meal deve ser inserido
+          const indexOfSection = storedMeals.findIndex(
+            (item: any) => item.title === mealSection
+          );
+          console.log(indexOfSection);
+          //se encontrou a section, insere o meal
+          if (indexOfSection !== -1) {
+            storedMeals[indexOfSection].data.push(meal);
+          } else {
+            //se não encontrou a section, cria a section e insere o meal
+            storedMeals.push({ title: mealSection, data: [meal] });
+          }
+        } else {
+          storedMeals[indexes.sectionIndex].data[indexes.mealIndex] = meal;
+        }
         await AsyncStorage.setItem(
           MEAL_COLLECTION,
           JSON.stringify(storedMeals)
         );
       }
-     }
+    }
   } catch (error) {
     throw error;
   }
