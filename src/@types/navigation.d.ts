8246cef45c781eecee227673474ddeecfb1cace4
LucasAlencar <lucas.linard@outlook.com>
2023-04-10 11:16:27 -0300

    feat: navigation rootParamList

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,12 +1,22 @@
-export declare global {
-    namespace ReactNavigation{
-        interface RootParamList{
-            Home: undefined;
-            Stats: undefined;
-            Details: undefined   ;
-            MealForm: undefined;
-            Feedback: undefined;
+import { MealType, MealFormType } from "./Meal";
 
-        }
+import { isOnDietSelectType } from "@components/Meal";
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      Stats: {
+        percent: number;
+      };
+      Details: MealType;
+      MealForm: {
+        type : MealFormType,
+        meal?: MealType
+
+      };
+      Feedback: {
+        isOnDiet: isOnDietSelectType;
+      };
     }
+  }
 }
