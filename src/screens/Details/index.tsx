2688d41d45850008c0d3fd4be84f3b395bbbf1e7
LucasAlencar <lucas.linard@outlook.com>
2023-07-04 11:05:21 -0300

    feat: Details modal for confirmation

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -8,28 +8,35 @@ import { Modal } from "@components/Modal";
 import { mealDeleteById } from "@storage/meal/mealDeleteById";
 import { MealType } from "src/@types/Meal";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 export default function Details() {
   const navigation = useNavigation();
-  const route  = useRoute();
+  const route = useRoute();
   const params = route.params as MealType;
-  const { t } = useTranslation(); 
-  const type = params.isOnDiet === "YES" ?  'EDIT-ON-DIET' : 'EDIT-OFF-DIET';  
+  const { t } = useTranslation();
 
-  const formattedDate = moment(params.date).format('DD/MM/YYYY HH:mm');
+  const type = params.isOnDiet === "YES" ? "EDIT-ON-DIET" : "EDIT-OFF-DIET";
+  const formattedDate = moment(params.date).format("DD/MM/YYYY HH:mm");
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   function handleEditMeal() {
     navigation.navigate("MealForm", { type, ...params });
   }
 
+  function handleToggleModal() {
+    setIsModalVisible(!isModalVisible);
+  }
+
   async function handleDeleteMeal() {
+    handleToggleModal();
     await mealDeleteById(params.id);
     navigation.navigate("Home");
   }
 
   return (
     <Container type={type} edges={["top", "left", "right"]}>
-      <Header title={t('Common:Meal')} />
+      <Header title={t("Common:Meal")} />
       <Card>
         <ContainerItem style={{ marginTop: 16 }}>
           <Text fontFamily="BOLD" fontSize="XL">
@@ -39,38 +46,42 @@ export default function Details() {
         </ContainerItem>
         <ContainerItem>
           <Text fontFamily="BOLD" fontSize="SM">
-          {t('Forms:DateAndTime')}
+            {t("Forms:DateAndTime")}
           </Text>
           <Text>{formattedDate}</Text>
         </ContainerItem>
         <Tag>
-          <Dot isOnDiet={params.isOnDiet}/>
+          <Dot isOnDiet={params.isOnDiet} />
           <Text fontSize="SM">
-            {params.isOnDiet === "YES" ? t('Common:OnDiet') :t('Common:OffDiet') }
-            </Text>
+            {params.isOnDiet === "YES"
+              ? t("Common:OnDiet")
+              : t("Common:OffDiet")}
+          </Text>
         </Tag>
         <EndBox>
           <Button
             Icon="PencilSimpleLine"
-            title={t('Button:Meal.Edit')}
+            title={t("Button:Meal.Edit")}
             fullWidth
             onPress={handleEditMeal}
           />
           <Button
             type="OUTLINE"
             Icon="Trash"
-            title={t('Button:Meal.Delete')}
+            title={t("Button:Meal.Delete")}
             fullWidth
-            onPress={handleDeleteMeal}
+            onPress={handleToggleModal}
           />
         </EndBox>
       </Card>
       <Modal
-        title={t('Feedback:WantToDeleteMeal')}
-        visible={false}
+        title={t("Feedback:WantToDeleteMeal")}
+        visible={isModalVisible}
         transparent
-        ConfirmButtonTitle={t('Button:DeleteConfirmation')}
-        CancelButtonTitle={t('Button:Cancel')}
+        onConfirm={handleDeleteMeal}
+        onCancel={handleToggleModal}
+        ConfirmButtonTitle={t("Button:DeleteConfirmation")}
+        CancelButtonTitle={t("Button:Cancel")}
       />
     </Container>
   );
