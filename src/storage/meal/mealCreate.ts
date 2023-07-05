4ae1b9d2de5c8e92753465f65acd31115048fd98
LucasAlencar <lucas.linard@outlook.com>
2023-07-05 13:41:30 -0300

    refactor: Create meal object

--- src/storage/meal/mealCreate.ts
+++ src/storage/meal/mealCreate.ts
@@ -1,9 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MEAL_COLLECTION } from "@storage/storageConfig";
 import { MealType } from "src/@types/Meal";
+import moment from "moment";
 
 type MealStorageType = {
-  title: string;
+  day: Date;
   data: MealType[];
 };
 export async function mealCreate(meal: MealType) {
@@ -13,10 +14,10 @@ export async function mealCreate(meal: MealType) {
       // value previously stored
       const storage: MealStorageType[] = JSON.parse(storedMeals);
 
-      const mealSection = meal.date.toLocaleDateString("pt-BR");
+      const mealSection = meal.date;
 
-      const indexOfSection = storage.findIndex(
-        (item: any) => item.title === mealSection
+      const indexOfSection = storage.findIndex((item: any) =>
+        moment(item.day).isSame(moment(mealSection), "day")
       );
 
       if (indexOfSection !== -1) {
@@ -31,28 +32,24 @@ export async function mealCreate(meal: MealType) {
         await AsyncStorage.setItem(MEAL_COLLECTION, JSON.stringify(storage));
       } else {
         const indexToInsertSection = storage.findIndex(
-          (item) =>
-            new Date(meal.date).getTime() >
-            new Date(
-              item.title.split("/").reverse().join("-") + "T00:00:00-03:00"
-            ).getTime()
+          (item) => new Date(meal.date).getTime() > new Date(item.day).getTime()
         );
         if (indexToInsertSection !== -1) {
           storage.splice(indexToInsertSection, 0, {
-            title: mealSection,
+            day: mealSection,
             data: [meal],
           });
         } else {
-          storage.push({ title: mealSection, data: [meal] });
+          storage.push({ day: mealSection, data: [meal] });
         }
         await AsyncStorage.setItem(MEAL_COLLECTION, JSON.stringify(storage));
       }
     } else {
       // first time storing
-      const mealSection = meal.date.toLocaleDateString("pt-BR");
+      const mealSection = meal.date;
       await AsyncStorage.setItem(
         MEAL_COLLECTION,
-        JSON.stringify([{ title: mealSection, data: [meal] }])
+        JSON.stringify([{ day: mealSection, data: [meal] }])
       );
     }
   } catch (error) {
